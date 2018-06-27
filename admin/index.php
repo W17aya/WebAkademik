8adb45183806ecb55a782d31799018358213b8d6
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-06-27 10:36:17 +0800

    Penggunaan Menu Login

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<html>
+<div class="login_form">
+<center>
+<table>
+
+<tr align='center'>
+    <td height ='20px'>
+    <font face='verdana' size='4' color='#a74332b' align='center'>
+    --LOGIN ADMIN--
+     
+</td>
+</tr>
+<tr>
+<td>
+<form name='LOGIN' method='post' action="AdminValidate.php">
+<table border=0>
+<tr>
+<td width ='90px'><label>USERNAME:> </label></td>
+<td><input size='35' type='text' name-'USERNAME' ></td>
+</tr> 
+</table>
+</html>
