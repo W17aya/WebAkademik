5299ea55a753eab4829d5e8dbfdda53c0699f35d
Eko Wijaya <32318250+W17aya@users.noreply.github.com>
2018-06-28 12:34:31 +0800

    admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,6 @@
 <html>
+...................................................................
+...................................................................
 <div class="login_form">
 <center>
 <table>
@@ -17,6 +19,25 @@
 <tr>
 <td width ='90px'><label>USERNAME:> </label></td>
 <td><input size='35' type='text' name-'USERNAME' ></td>
+</tr>
+<br>
+<tr>
+<td><label>Password:</label> </td>
+<td>
+<input size="35" YPE='password' name='PASSWORD_ADMIN'></td>
 </tr> 
+<tr>
+
+<td></td>
+<td>
+<input type='SUBMIT' name='SUBMIT' value='LOGIN'>
+</td>
+</tr>
 </table>
+</form>
+</td>
+</tr>
+</table>
+</center>
+.....................................................................................
 </html>
